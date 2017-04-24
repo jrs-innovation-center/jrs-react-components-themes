1dba0ba86d9a735a0605d6dedcd5ec73ee3d6d88
tripott <tripott@gmail.com>
2017-04-24 17:11:15 -0400

    Create dark.js

---
+++ dark.js
@@ -0,0 +1,10 @@
+const dark = {
+  themeName: 'dark',
+  themeStyles: {
+    panelBorder: 'ba bg-black-50',
+    panelBackgroundColor: 'bg-black-50',
+    panelTextColor: 'white'
+  }
+}
+
+export default dark
