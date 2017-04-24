ffe3c538331d5b443f4dc164214ace4f444a35ea
tripott <tripott@gmail.com>
2017-04-24 17:10:20 -0400

    Create light.js

---
+++ light.js
@@ -0,0 +1,10 @@
+const light = {
+  themeName: 'light',
+  themeStyles: {
+    panelBorder: 'ba b--dark-gray',
+    panelBackgroundColor: 'bg-white',
+    panelTextColor: 'black-50'
+  }
+}
+
+export default light
